59b98818575e9c6dd35def9dacefb187cc986bd9
Peter Hoang <phonghoang@gmail.com>
2015-10-03 09:31:13 -0700

    test commit

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,24 +1,18 @@
 class EventsController < ApplicationController
 
   def index
-    @events = Event.all
-    current_user
 
-    render :index
   end
 
   def new
-    @event = Event.new
   end
 
   def show
-    @event = Event.find(params[:id])
-    render :show
+
   end
 
   def create
-    @event = Event.create(event_params)
-    redirect_to '/events'
+
   end
 
   def edit
@@ -28,12 +22,6 @@ class EventsController < ApplicationController
   end
 
   def destroy
-  end
-
-    private
-
-  def event_params
-    params.require(:library).permit(:name, :address, :start_date, :start_time, :max_headcount, :current_headcount, :description)
   end
 
 end
