a941747d426e49156724dd6e3f2f801da39ea813
James <jkbennington@gmail.com>
2015-10-05 14:55:34 -0700

    added show for city

--- db/schema.rb
+++ db/schema.rb
@@ -45,11 +45,6 @@ ActiveRecord::Schema.define(version: 20151005213935) do
     t.integer  "sport"
   end
 
-  create_table "sports", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
