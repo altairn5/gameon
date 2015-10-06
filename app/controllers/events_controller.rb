e2a5a97711ac6cd381e27c6b1ecf32844eb91916
James <jkbennington@gmail.com>
2015-10-05 18:30:51 -0700

    added creator logic

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,6 +14,7 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
+    @creator = "#{User.find(@event.user_id).first_name} #{User.find(@event.user_id).last_name}"
     render :show
   end
 
