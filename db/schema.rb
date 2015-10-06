2cc1f899ade2689d94b0d7887c9b9fa6782708a1
Humberto Calero <altairn5@uga.edu>
2015-10-06 11:35:35 -0700

    testing remove join button

--- db/schema.rb
+++ db/schema.rb
@@ -47,6 +47,11 @@ ActiveRecord::Schema.define(version: 20151006182628) do
     t.time     "time"
   end
 
+  create_table "sports", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
