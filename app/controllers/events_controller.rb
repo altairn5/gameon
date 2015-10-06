554e3e7b56bbabc3ccc287f1a3291cdb6e9f29bd
Humberto Calero <altairn5@uga.edu>
2015-10-05 20:56:54 -0700

    fixing

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,9 +14,8 @@ class EventsController < ApplicationController
   end
 
   def show
-
     @event = Event.find(params[:id])
-    @creator = "#{User.find(@event.user_id).first_name} #{User.find(@event.user_id).last_name}"
+    @creator = User.find(@event.user_id)
     id = params[:id]
     @event = Event.find(params[:id])
     render :show
