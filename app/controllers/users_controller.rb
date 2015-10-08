09db6c313c133a9e5ff372a21253c21ed2dd3281
James <jkbennington@gmail.com>
2015-10-08 16:37:16 -0700

    fix active record association of user and city

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,9 @@ def index
   def show
     @user = User.find(params[:id])
     @events = @user.events
-    @titles = @user.events.name
+    @local = @user.city.events
+    # @title = @local.all
+    # @words = @
     @is_creator = Event.where(user_id: @user.id)
     @event = Event.all
     @current_user = current_user
